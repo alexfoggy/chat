2d948678caf90ee5440b58c7f2bf022d3b848138
Igor Ejov <maxejj@gmail.com>
2022-03-18 14:52:32 +0200

    up

--- app/Http/Controllers/User/RecordController.php
+++ app/Http/Controllers/User/RecordController.php
@@ -100,8 +100,8 @@ class RecordController extends Controller
         if ($user) {
 
             $responseMsg = Msg::where('user_id', $user->id)->where('userStatus', 2)->where('sendStatus', 0)->orderBy("created_at", 'ASC')->get();
-            dd($responseMsg);
-            if (count($responseMsg) > 0) {
+
+            if ($responseMsg->isNotEmpty()) {
                 Msg::whereIn('id', $responseMsg->pluck('id'))->update(['sendStatus' => 1]);
                 //$responseMsg->update(['sendStatus'=>1]);
                 return response()->json([
